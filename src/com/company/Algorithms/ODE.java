f836d165a8bac78a1fd24afc9d1f8adf9c66be91
SergeyHovh <sergohovhyan@gmail.com>
2018-11-16 11:34:25 +0400

    added minor helper methods -ODE --- add, subtract -Main --- getPoints

--- src/com/company/Algorithms/ODE.java
+++ src/com/company/Algorithms/ODE.java
@@ -1,10 +1,22 @@
 package com.company.Algorithms;
 
 public interface ODE {
+    static ODE add(ODE f1, ODE f2) {
+        return (x, y) -> f1.derivative(x, y) + f2.derivative(x, y);
+    }
+
     /**
      * @param x initial position
      * @param y initial values of y, y', y'' ...
      * @return
      */
     double derivative(double x, double... y);
+
+    default ODE add(ODE ode) {
+        return (x, y) -> this.derivative(x, y) + ode.derivative(x, y);
+    }
+
+    default ODE subtract(ODE ode) {
+        return (x, y) -> this.derivative(x, y) - ode.derivative(x, y);
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.Algorithms.ODE;
 import com.company.Algorithms.RK4;
 
 import java.io.FileNotFoundException;
@@ -12,16 +13,24 @@ public class Main {
     public static final double G = 9.8;
 
     public static void main(String[] args) {
+        getPoints(0, new double[]{1, 0}, (x, y) -> -5 * Math.sin(y[0]));
+
+    }
+
+    private static void getPoints(double x0, double[] y0, ODE ode) {
         RK4 rk4 = new RK4();
         Formatter file;
-        double iter = 0.0, R = 1;
+        double iter = 0.0;
+        double[] before = new double[y0.length];
+        System.arraycopy(y0, 0, before, 0, before.length);
         try {
             file = new Formatter(PATH);
             file.flush();
             while (iter < 10) {
-                double v = rk4.solveSecondOrder(0, 1, 0, iter, (x, y) -> -G / R * Math.sin(y[0]));
+                double v = rk4.solveHighOrder(x0, y0, iter, ode);
                 file.format("%s", v + "\n");
                 iter += 0.01;
+                System.arraycopy(before, 0, y0, 0, before.length);
             }
             file.close();
         } catch (FileNotFoundException e) {
