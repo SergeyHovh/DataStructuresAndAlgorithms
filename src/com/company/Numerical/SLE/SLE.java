11266a1928f232690b3d71912dd3b4f6783483c5
SergeyHovh <sergohovhyan@gmail.com>
2018-12-21 00:23:26 +0400

    added Gauss-Jordan elimination method

---
+++ src/com/company/Numerical/SLE/SLE.java
@@ -0,0 +1,116 @@
+package com.company.Numerical.SLE;
+
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+
+public class SLE {
+    private double[][] coefficients;
+    private double[] B;
+
+    SLE(double[][] coefficients, double[] B) {
+        this.coefficients = coefficients;
+        this.B = B;
+    }
+
+    public void eliminate() {
+        eliminate(coefficients);
+    }
+
+    private void eliminate(double[][] A) {
+        int length = A.length;
+        for (int i = 1; i < length; i++) {
+            for (int j = i; j < length; j++) {
+                if (abs(A[j][i - 1]) <= 1.0E-12) continue;
+                addRows(i - 1, j, -A[j][i - 1] / A[i - 1][i - 1]);
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            multiplyRow(i, 1 / A[i][i]);
+        }
+    }
+
+    public void addRows(int source, int dest, double coefficient) {
+        int length = coefficients.length;
+        for (int i = 0; i < length; i++) {
+            coefficients[dest][i] += coefficients[source][i] * coefficient;
+        }
+        B[dest] += B[source] * coefficient;
+    }
+
+    public void multiplyRow(int i, double coefficient) {
+        int length = coefficients.length;
+        for (int j = 0; j < length; j++) {
+            coefficients[i][j] *= coefficient;
+        }
+        B[i] *= coefficient;
+    }
+
+    public double determinant() {
+        return determinant(this.coefficients);
+    }
+
+    public SLE put(double[] x, int i) {
+        return put(this, x, i);
+    }
+
+    private SLE put(SLE A, double[] x, int i) {
+        int length = A.coefficients.length;
+        double[][] temp = new double[length][length];
+        for (int j = 0; j < length; j++) {
+            System.arraycopy(A.coefficients[j], 0, temp[j], 0, length); // update
+        }
+        for (int j = 0; j < temp.length; j++) {
+            temp[j][i] = x[j];
+        }
+        return new SLE(temp, A.B);
+    }
+
+    private double determinant(double[][] A) {
+        double det = 0;
+        if (A.length != A[0].length) return Double.MIN_VALUE;
+        if (A.length == 2)
+            return A[0][0] * A[1][1] - A[0][1] * A[1][0];
+        else {
+            for (int i = 0; i < A.length; i++) {
+                if (abs(A[i][0]) <= 1.0E-10) continue;
+                det += pow(-1, i) * A[i][0] * determinant(reduce(A, i, 0));
+            }
+            return det;
+        }
+    }
+
+    private double[][] reduce(double[][] A, int i, int j) {
+        double[][] aPrime = new double[A.length - 1][A.length - 1];
+        int length = aPrime.length;
+        int length1 = aPrime[0].length;
+        for (int m = 0; m < length; m++) {
+            for (int n = 0; n < length1; n++) {
+                if (m <= i && n <= j) aPrime[m][n] = A[m][n];
+                if (m >= i && n <= j) aPrime[m][n] = A[m + 1][n];
+                if (m <= i && n >= j) aPrime[m][n] = A[m][n + 1];
+                if (m >= i && n >= j) aPrime[m][n] = A[m + 1][n + 1];
+            }
+        }
+        return aPrime;
+    }
+
+    public double[][] getCoefficients() {
+        return coefficients;
+    }
+
+    public double[] getB() {
+        return B;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder print = new StringBuilder();
+        for (int i = 0, coefficientsLength = coefficients.length; i < coefficientsLength; i++) {
+            double[] coefficient = coefficients[i];
+            print.append(Arrays.toString(coefficient)).append(" | ").append(B[i]).append("\n");
+        }
+        return print.toString();
+    }
+}
--- src/com/company/Numerical/SLE/SLESolver.java
+++ src/com/company/Numerical/SLE/SLESolver.java
@@ -1,80 +1,36 @@
 package com.company.Numerical.SLE;
 
-import static java.lang.Math.pow;
-
 public class SLESolver {
-    private static double determinant(double[][] A) {
-        double det = 0;
-        if (A.length != A[0].length) return Double.MIN_VALUE;
-        if (A.length == 2) {
-            return A[0][0] * A[1][1] - A[1][0] * A[0][1]; // 2X2 matrix determinant
-        } else {
-            for (int i = 0; i < A.length; i++) {
-                if (A[i][0] == 0) continue;
-                det += pow(-1, i) * A[i][0] * determinant(reduce(A, i, 0));
-            }
-            return det;
-        }
-    }
-
-    private static double[][] reduce(double[][] A, int i, int j) {
-        double[][] aPrime = new double[A.length - 1][A.length - 1];
-        int length = aPrime.length;
-        int length1 = aPrime[0].length;
-        for (int m = 0; m < length; m++) {
-            for (int n = 0; n < length1; n++) {
-                if (m <= i && n <= j) aPrime[m][n] = A[m][n];
-                if (m >= i && n <= j) aPrime[m][n] = A[m + 1][n];
-                if (m <= i && n >= j) aPrime[m][n] = A[m][n + 1];
-                if (m >= i && n >= j) aPrime[m][n] = A[m + 1][n + 1];
-            }
-        }
-        return aPrime;
-    }
 
     /**
-     * replace i-th column of A by b
-     *
-     * @param A initial matrix
-     * @param b desired column
-     * @param i column number
-     * @return modified matrix
+     * Solves Ax=b by Cramer's rule (works fast for up to 20 equations)
      */
-    private static double[][] put(double[][] A, double[] b, int i) {
-        for (int j = 0; j < A[0].length; j++) {
-            A[j][i] = b[j];
-        }
-        return A;
-    }
-
-    /**
-     * Solves Ax=b by Cramer's rule (works fast for up to 8 equations)
-     */
-    public static double[] solveCramer(double[][] A, double[] b) {
+    public static double[] solveCramer(SLE system) {
+        system.eliminate();
+        double[] b = system.getB();
         int length = b.length;
-        double[][] before = new double[length][length];
         double[] X = new double[length];
-        double determinant = determinant(A); // computing the determinant of the original matrix
-        for (int i = 0; i < before.length; i++) {
-            System.arraycopy(A[i], 0, before[i], 0, length); // update
-        }
+        double determinant = system.determinant(); // computing the determinant of the original matrix
         for (int i = 0; i < length; i++) {
-            double[][] put = put(A, b, i);
-            X[i] = determinant(put) / determinant;
-            for (int j = 0; j < before.length; j++) {
-                System.arraycopy(before[j], 0, A[j], 0, length); // update
-            }
+            SLE p = system.put(b, i);
+            X[i] = p.determinant() / determinant;
         }
         return X;
     }
 
-    static double[] matrixVectorMultiply(double[][] A, double[] x) {
-        double[] X = new double[x.length];
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A.length; j++) {
-                X[i] += A[i][j] * x[j];
+    /**
+     * solves Ax=b by Gauss-Jordan elimination rule (very fast)
+     */
+    public static double[] solveGaussJordan(SLE system) {
+        system.eliminate();
+        double[][] A = system.getCoefficients();
+        int length = A.length;
+
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                system.addRows(i + 1, j, -A[j][i + 1]);
             }
         }
-        return X;
+        return system.getB();
     }
 }
