0c4ef68567caf204df9d745d6d0543d0f2d6802b
SergeyHovh <sergohovhyan@gmail.com>
2019-01-04 18:37:34 +0400

    added rotate method using complex numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,14 @@
 package com.company;
 
-import com.company.Numerical.ODE.Embedded.RKDP;
-import com.company.Numerical.ODE.Embedded.RKF45;
-import com.company.Numerical.ODE.Explicit.RK438;
+import com.company.Numerical.Fourier.Transform.ComplexNumbers;
 import com.company.Numerical.ODE.ODESolver;
 import com.company.Numerical.ODE.ODESystem;
 
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Formatter;
 
-import static java.lang.Math.*;
+import static java.lang.Math.expm1;
+import static java.lang.Math.toRadians;
 
 public class Main {
     public static final String PATH = "src/com/company/data.txt";
@@ -18,27 +16,22 @@ public class Main {
     public static final double G = 9.8;
 
     public static void main(String[] args) {
-        ODESolver rkClassic = new RK438();
-        ODESolver rkf45 = new RKF45();
-        ODESolver rkdp = new RKDP();
-        double[][] initialConditions = {
-                {1, 1}, // first equation
-                {1, 2}  // second equation
-        };
-        ODESystem[] system = {
-                (x, y) -> sqrt(y[1][0]),        // first equation
-                (x, y) -> 4 * pow(y[0][0], 2)   // second equation
-        };
-        double[][] rkClassicResult = rkClassic.solveHighOrder(0, initialConditions, 0.1, system);
-        System.out.println(Arrays.deepToString(rkClassicResult));
-//        System.out.println(E + " " + pow(E, 2) + " " + 2 * pow(E, 2));
+
+    }
+
+    private static double[] rotate(double x, double y, double phi) {
+        ComplexNumbers current = new ComplexNumbers(x, y);
+        ComplexNumbers multiply = current.multiply(ComplexNumbers.eToIX(toRadians(phi)));
+        return new double[]{multiply.getReal(), multiply.getImaginary()};
     }
 
     private static void getPoints(double x0, double[][] y0, double upTo, ODESystem[] ode, ODESolver ODESolver) {
         Formatter file;
         double iter = 0.0;
         double[][] before = new double[y0.length][y0[0].length];
-        System.arraycopy(y0, 0, before, 0, before.length);
+        for (int i = 0; i < y0.length; i++) {
+            System.arraycopy(y0[i], 0, before[i], 0, before.length);
+        }
         try {
             file = new Formatter(PATH);
             file.flush();
